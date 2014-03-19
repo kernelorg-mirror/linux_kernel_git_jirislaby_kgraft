adff9df4bb71ed296716bb4362dba0a3b654024d
Jiri Slaby <jslaby@suse.cz>
2014-03-19 15:37:06 +0100

    ftrace: s390, move arch fentry_* stuff to a separate header
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

---
+++ arch/s390/include/asm/fentry.h
@@ -0,0 +1,29 @@
+#ifndef _ASM_S390_FENTRY_H
+#define _ASM_S390_FENTRY_H
+
+#ifdef CONFIG_64BIT
+#define MCOUNT_INSN_SIZE  12
+#else
+#define MCOUNT_INSN_SIZE  22
+#endif
+
+#ifdef CONFIG_FENTRY_RECORD_LIB
+
+#ifndef __ASSEMBLY__
+
+extern void _mcount(void);
+
+struct fentry_arch { };
+
+#define MCOUNT_ADDR ((long)_mcount)
+
+static inline unsigned long fentry_call_adjust(unsigned long addr)
+{
+	return addr;
+}
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* CONFIG_FENTRY_RECORD_LIB */
+
+#endif /* _ASM_S390_FENTRY_H */
