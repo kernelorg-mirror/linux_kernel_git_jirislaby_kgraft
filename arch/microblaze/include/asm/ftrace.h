89b642212603b9e4c02df29c4015f5c9e5d0f1e4
Jiri Slaby <jslaby@suse.cz>
2014-03-19 15:37:06 +0100

    ftrace: microblaze, move arch fentry_* stuff to a separate header
    Signed-off-by: Jiri Slaby <jslaby@suse.cz>

--- arch/microblaze/include/asm/ftrace.h
+++ arch/microblaze/include/asm/ftrace.h
@@ -1,26 +1,13 @@
 #ifndef _ASM_MICROBLAZE_FTRACE
 #define _ASM_MICROBLAZE_FTRACE
 
+#include <asm/fentry.h>
+
 #ifdef CONFIG_FUNCTION_TRACER
 
-#define MCOUNT_ADDR		((long)(_mcount))
-#define MCOUNT_INSN_SIZE	8 /* sizeof mcount call */
-
 #ifndef __ASSEMBLY__
-extern void _mcount(void);
 extern void ftrace_call_graph(void);
 #endif
-
-#ifdef CONFIG_DYNAMIC_FTRACE
-/* reloction of mcount call site is the same as the address */
-static inline unsigned long fentry_call_adjust(unsigned long addr)
-{
-	return addr;
-}
-
-struct fentry_arch {
-};
-#endif /* CONFIG_DYNAMIC_FTRACE */
 
 #endif /* CONFIG_FUNCTION_TRACER */
 #endif /* _ASM_MICROBLAZE_FTRACE */
